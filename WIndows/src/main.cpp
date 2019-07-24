e24aa07aa4d29c8b24279c4ebb82ad55b5c69e43
Joshua E. Roybal <developer@joshroybal.com>
2019-07-24 11:56:53 -0500

    Add files via upload
    Design changes, program now prints summary for each security before printing merged data summary.

--- WIndows/src/main.cpp
+++ WIndows/src/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <iomanip>
+#include <string>
 #include <map>
 #include <vector>
 #include "csv.hpp"
 #include "stats.hpp"
-
-void statReport(const Stats&, const Stats&);
 
 int main(int argc, char *argv[])
 {
@@ -15,15 +13,16 @@ int main(int argc, char *argv[])
       std::cerr << "Usage: " << argv[0] << "file1 file2\n";
       return 1;
    }
+	
+	std::string infile1(argv[1]);
+	std::string infile2(argv[2]);
 
-   std::vector<double> x;
-   std::vector<double> y;
    std::map<std::string, double> xmap;
    std::map<std::string, double> ymap;
    std::string line;
    std::ifstream ifstr;
 
-   ifstr.open(argv[1], std::ifstream::in);
+   ifstr.open(infile1.c_str(), std::ifstream::in);
    if (ifstr.fail()) {
       std::cerr << "failure opening file " << argv[1] << std::endl;
       return 1;
@@ -41,7 +40,7 @@ int main(int argc, char *argv[])
    }
    ifstr.close();
 
-   ifstr.open(argv[2], std::ifstream::in);
+   ifstr.open(infile2.c_str(), std::ifstream::in);
    if (ifstr.fail()) {
       std::cerr << "failure opening file " << argv[2] << std::endl;
       return 1;
@@ -59,9 +58,24 @@ int main(int argc, char *argv[])
    }
    ifstr.close();
 
+   std::vector<double> x;
+   std::vector<double> y;
 
    std::map<std::string, double>::iterator xmit;
    std::map<std::string, double>::iterator ymit;
+	
+	// first process each data set individually
+	for (xmit = xmap.begin(); xmit != xmap.end(); xmit++) 
+		x.push_back(xmit->second);
+
+   for (ymit = ymap.begin(); ymit != ymap.end(); ymit++)
+		y.push_back(ymit->second);
+	
+	Stats xStats(x);
+	Stats yStats(y);	
+	
+	x.clear();
+	y.clear();
 
    for (xmit = xmap.begin(); xmit != xmap.end(); xmit++) {
       ymit = ymap.find(xmit->first);
@@ -72,89 +86,16 @@ int main(int argc, char *argv[])
    }
 
    coStats xyStats(x, y);
-   std::cout << "n = " << x.size() << std::endl;
-   std::cout << "cov(X,Y) = " << xyStats.CoVariance() << std::endl;
-   std::cout << "corr(X,Y) = " << xyStats.Correlation() << std::endl;
-   std::cout << std::endl;
-   statReport(xyStats.xStats, xyStats.yStats);
+
+	const std::string label1("X = " + infile1.substr(0, infile1.length()-4));
+	const std::string label2("Y = " + infile2.substr(0, infile2.length()-4));
+
+	// display x data summary
+	xStats.displayReport(label1);
+	// display y data summary
+	yStats.displayReport(label2);
+	// display merged data summary
+   xyStats.displayReport();
 
    return 0;
-}
-
-void statReport(const Stats& x, const Stats& y)
-{
-   const int w = 12;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "STAT";
-   std::cout << std::right;
-   std::cout << std::setw(w) << "X" << std::setw(w) << "Y" << std::endl;
-
-   std::cout << std::fixed << std::setprecision(3);
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "minimum";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.Minimum();
-   std::cout << std::setw(w) << y.Minimum() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "maximum";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.Maximum();
-   std::cout << std::setw(w) << y.Maximum() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "median";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.Median();
-   std::cout << std::setw(w) << y.Median() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "mean";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.Mean();
-   std::cout << std::setw(w) << y.Mean() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "popvar";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.PopVar();
-   std::cout << std::setw(w) << y.PopVar() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "popstd";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.PopStd();
-   std::cout << std::setw(w) << y.PopStd() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "samvar";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.SamVar();
-   std::cout << std::setw(w) << y.SamVar() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "samstd";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.SamStd();
-   std::cout << std::setw(w) << y.SamStd() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "mean deviation";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.MeanDeviation();
-   std::cout << std::setw(w) << y.MeanDeviation() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "median deviation";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.MedianDeviation();
-   std::cout << std::setw(w) << y.MedianDeviation() << std::endl;
-
-   std::cout << std::left;
-   std::cout << std::setw(20) << "skewness";
-   std::cout << std::right;
-   std::cout << std::setw(w) << x.Skewness();
-   std::cout << std::setw(w) << y.Skewness() << std::endl;
 }
